647872715303bb0568ea266cb76676f69109d41b
sajjad <sajjadsajjad098765@gmail.com>
2024-09-13 21:48:34 +0600

    server running successfully

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.get("/", (req, res) => {
-    res.send("hello.............");
+    res.send("hello............. Banhgsgh");
 });
 app.listen(port, () => {
     console.log(`Example ${port}`);
