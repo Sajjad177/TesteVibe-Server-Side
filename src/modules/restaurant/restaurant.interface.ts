f48bdc046077939f10fe7d4ffaa9994cbeac9cb7
sajjad <sajjadsajjad098765@gmail.com>
2024-09-13 23:06:35 +0600

    restaurant interface and schema added

---
+++ src/modules/restaurant/restaurant.interface.ts
@@ -0,0 +1,17 @@
+import mongoose, { Document } from "mongoose";
+
+export interface IRestaurant {
+  user: mongoose.Schema.Types.ObjectId;
+  restaurantName: string;
+  city: string;
+  country: string;
+  deliveryTime: number;
+  cuisines: string[];
+  imageUrl: string;
+  menus: mongoose.Schema.Types.ObjectId[];
+}
+
+export interface IRestaurantDocument extends IRestaurant, Document {
+  createAt: Date;
+  updateAt: Date;
+}
