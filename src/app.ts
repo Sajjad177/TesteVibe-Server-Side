8b990ec294466c061bcb3bfb22f1b2533a6baf94
sajjad <sajjadsajjad098765@gmail.com>
2024-09-15 00:21:12 +0600

    user route added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,26 @@
 import express, { Request, Response } from "express";
+import { UserRouters } from "./modules/user/user.route";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
+
+// setup parser
+app.use(bodyParser.json({ limit: "10mb" })); //added limit data -> 10mb
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
+
+//corse setup :
+const corsOptions = {
+  origin: "http://localhost:8000",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+//router
+app.use("/api/v1/user", UserRouters);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World! The food server is running");
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,15 @@
+import { isAuthenticated } from "./../../middlewares/isAuthenticated";
+import express from "express";
+import { UserController } from "./user.controller";
+
+const router = express.Router();
+
+router.get("/check-auth", isAuthenticated, UserController.checkAuth);
+router.post("/signup", UserController.signup);
+router.post("/login", UserController.login);
+router.post("/verify-email", UserController.VerifyEmail);
+router.post("/forget-password", UserController.forgetPassword);
+router.post("/reset-password/:token", UserController.resetPassword);
+router.put("/profile/update", isAuthenticated, UserController.updateProfiles);
+
+export const UserRouters = router;
